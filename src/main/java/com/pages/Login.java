48cb944fbe6018793a1d2eaed637a06132e8bfed
usreddy <usreddy9@hotmail.com>
2024-04-25 09:07:53 -0500

    first commit

---
+++ src/main/java/com/pages/Login.java
@@ -0,0 +1,9 @@
+package com.pages;
+
+public class Login {
+	
+	public void loginPage() {
+		System.out.println("This is login page");
+	}
+
+}
---
+++ src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,9 @@
+package com.testcases;
+
+public class LoginTest {
+	public void loginPage() {
+		System.out.println("This is login test page");
+	}
+
+
+}
